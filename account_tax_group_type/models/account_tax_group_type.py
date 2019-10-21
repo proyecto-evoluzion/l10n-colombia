f4499d477264e8bdaed474f7387723361ebc7fb8
joanmarin <joanmarinbustamante@gmail.com>
2019-10-21 16:02:32 -0500

    new modules

---
+++ account_tax_group_type/models/account_tax_group_type.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@joanmarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models, _
+
+
+class AccountTaxGroupType(models.Model):
+    _name = 'account.tax.group.type'
+
+    code = fields.Char(string='Code', required=True)
+    name = fields.Char(string='Name', required=True)
+    description = fields.Char(string='Description')
+
+    _sql_constraints = [(
+        'code_uniq',
+        'unique (code)',
+        _('The code of Tax Group Type must be unique!'))]
---
+++ l10n_co_account_tax_group_type/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@joanmarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Colombian Tax Group Types",
+    "summary": "Colombian Tax Group Types",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://www.exaap.com",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@joanmarin",
+    "category": "Localization",
+    "depends": [
+        "account_tax_group_menu",
+    ],
+    "data": [
+        "data/account_tax_group_type_data.xml"
+    ],
+    "installable": True,
+}
