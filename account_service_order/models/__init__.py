e6d11d993c18199b6389f6f10f1c7812417e0ba3
joanmarin <joanmarinbustamante@gmail.com>
2019-12-09 16:13:58 -0500

    [ADD] multicompany and code refactoring

--- account_service_order/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 Joan Marín <Github@JoanMarin>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
-
-import account_account
-import account_invoice_line
-import account_invoice
-import account_move_line
-import account_service_order
