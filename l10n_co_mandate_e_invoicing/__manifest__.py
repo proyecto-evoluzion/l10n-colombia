d7956b6b631fc2292a4c4b9067788d03954746d4
joanmarin <joanmarinbustamante@gmail.com>
2019-12-27 17:23:45 -0500

    [MOD] code refactoring of mandate and e_invoicing modules

---
+++ l10n_co_mandate_e_invoicing/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+	"name": "Colombian E-Invoicing Mandate",
+	"version": "10.0.1.0.0",
+	"website": "https://github.com/odooloco/l10n-colombia",
+	"author": "Joan Marín Github@JoanMarin",
+	"category": "Localization",
+	"summary": "Colombian E-Invoicing Mandate",
+	"depends": [
+		"l10n_co_mandate_account",
+		"l10n_co_account_e_invoicing",
+	],
+	"data": [],
+	"installable": True,
+}
