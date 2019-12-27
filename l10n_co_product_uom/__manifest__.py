67df73f316eb84cd043122b0aefea2fa27d96cdd
joanmarin <joanmarinbustamante@gmail.com>
2019-12-27 11:08:13 -0500

    [FIX] bugs [ADD] new module [MOD] improve e-invoicing

---
+++ l10n_co_product_uom/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+	"name": "Colombian Unit Code",
+	"version": "10.0.1.0.0",
+	"website": "https://github.com/odooloco/l10n-colombia",
+	"author": "Joan Marín Github@JoanMarin",
+	"category": "Localization",
+	"summary": "Colombian Unit Code",
+	"depends": [
+		"product",
+	],
+	"data": [
+		"security/ir.model.access.csv",
+		"data/product.uom.code.csv",
+		"views/product_uom_views.xml",
+	],
+	"installable": True,
+}
