ef4456f6c421838c94ed70c4346a24459b2b9e34
Capriatto <michu-1@hotmail.com>
2020-01-03 16:08:14 -0500

    [ADD] get number range for operation mode

--- l10n_co_account_e_invoicing/models/res_company.py
+++ l10n_co_account_e_invoicing/models/res_company.py
@@ -3,9 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from validators import url
-from global_functions import get_pkcs12
+from global_functions import get_pkcs12, get_xml_soap_with_signature, get_template_xml
 from odoo import api, models, fields, _
 from odoo.exceptions import ValidationError
+from requests import post, exceptions
+from lxml import etree
 
 
 class ResCompany(models.Model):
@@ -51,3 +53,41 @@ class ResCompany(models.Model):
         get_pkcs12(self.certificate_file, self.certificate_password)
 
         return rec
+
+    def getProductionData(self):
+        msg1 = _("Unknown Error,\nStatus Code: %s,\nReason: %s,\nContact with your administrator "
+                "or you can choose a journal with a Contingency Checkbook E-Invoicing sequence "
+                "and change the Invoice Type to 'Factura por Contingencia Facturador'.")
+        msg2 = _("Unknown Error: %s\nContact with your administrator "
+                "or you can choose a journal with a Contingency Checkbook E-Invoicing sequence "
+                "and change the Invoice Type to 'Factura por Contingencia Facturador'.")
+        b = "http://schemas.datacontract.org/2004/07/UploadDocumentResponse"
+        wsdl = "DIAN['wsdl-hab']"
+
+        SendBillAsync_values = self._get_SendBillAsync_values()
+        xml_soap_with_signature = get_xml_soap_with_signature(
+            get_template_xml(SendBillAsync_values, 'ProductionStage'),
+            SendBillAsync_values['Id'],
+            self.company_id.certificate_file,
+            self.company_id.certificate_password)
+        wsdl = "DIAN['wsdl']"
+
+        try:
+            response = post(
+                wsdl,
+                headers={'content-type': 'application/soap+xml;charset=utf-8'},
+                data=etree.tostring(xml_soap_with_signature))
+
+            if response.status_code == 200:
+                root = etree.fromstring(response.text)
+                return True
+            elif response.status_code in (500, 503, 507):
+                pass
+            else:
+                raise ValidationError(msg1 % (response.status_code, response.reason))
+
+            return False
+        except exceptions.RequestException as e:
+            raise ValidationError(msg2 % (e))
+
+        return False
