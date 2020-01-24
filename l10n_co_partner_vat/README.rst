85f251ac48befd64e0dca7b71395e316f360aed5
joanmarin <joanmarinbustamante@gmail.com>
2020-01-24 12:09:38 -0500

    [ADD] 12.0 l10n_co_partner_vat

---
+++ l10n_co_partner_vat/README.rst
@@ -0,0 +1,27 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+=================
+Base Vat Colombia
+=================
+
+- Types of identification document identified by the DIAN for the Colombian
+    localization, information obtained from the technical annex for e-invoicing
+    version 1.8, point 13.2.1.
+- Validate NIT with Verification Digit.
+
+- Tipos de documento de identificación identificados por la DIAN para la
+    localizacion Colombiana, informacion obtenida del anexo tecnico para
+    facturacion electronica version 1.8, punto 13.2.1.
+- Validar NIT con el dígito de verificación.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Joan Marín <https://github.com/JoanMarin>
+* Guillermo Montoya <https://github.com/guillermm>
---
+++ l10n_co_partner_vat/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# Copyright 2018 Guillermo Montoya <Github@guillermm>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner VAT Colombia",
+    "summary": "Module for Type of Identification Document and Colombian NIT Checking.",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/odooloco/l10n-colombia",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin, "
+              "Guillermo Montoya Github@guillermm",
+    "category": "Localization",
+    "depends": [
+        "base_vat",
+        "account"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/res_partner_document_type_data.xml", 
+        "views/res_partner_views.xml"
+    ],
+    "installable": True,
+}
