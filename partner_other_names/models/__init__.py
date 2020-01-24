40ebb98c3677282252c6a009f321cd44a01b784d
joanmarin <joanmarinbustamante@gmail.com>
2020-01-24 10:33:22 -0500

    [FIX] base_config_settings > res_config_settings

--- partner_other_names/models/__init__.py
+++ partner_other_names/models/__init__.py
@@ -6,6 +6,6 @@
 # Copyright 2018 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import base_config_settings
+from . import res_config_settings
 from . import res_partner
 from . import res_users
