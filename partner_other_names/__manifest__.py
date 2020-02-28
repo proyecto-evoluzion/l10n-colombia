f87ca672d9f667697135bd5b5a65340691ece234
joanmarin <joanmarinbustamante@gmail.com>
2020-02-28 11:25:29 -0500

    [REF] partner_other_names module

--- partner_other_names/__manifest__.py
+++ partner_other_names/__manifest__.py
@@ -2,8 +2,8 @@
 # Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2015 Antiun Ingenieria S.L. - Antonio Espinosa
 # Copyright 2017 Tecnativa - Pedro M. Baeza
-# Copyright 2018 EXA Auto Parts S.A.S Guillermo Montoya <Github@guillermm>
-# Copyright 2018 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
+# Copyright 2018 Guillermo Montoya <Github@guillermm>
+# Copyright 2018 Joan Marín <Github@JoanMarin>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -11,9 +11,8 @@
     "summary": "Have split first and other names",
     "version": "11.0.1.0.0",
     "license": "AGPL-3",
-    "website": "https://github.com/odooloco/l10n-colombia",
+    "website": "https://github.com/OCA/partner-contact",
     "author": "EXA Auto Parts Github@exaap, "
-              "Joan Marín Github@JoanMarin, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
     "category": "Partner Management",
